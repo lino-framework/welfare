c4dfc1ff22de473cf5a1fac68bc5e0c89a71fc9d
Luc Saffre <luc.saffre@mail.ee>
2012-12-01 23:52:09 -0400

    http://code.google.com/p/lino/source/browse/docs/blog/2012/1201.rst

--- docs/de/releases/index.rst
+++ docs/de/releases/index.rst
@@ -3,11 +3,13 @@ Releases
 
 - :doc:`coming` : Planung hat begonnen
 
-- :doc:`1.0.4` : in Betrieb seit dem 27.11.2012
+- :doc:`1.0.5` : in Betrieb seit dem 01.12.2012
 
-- :doc:`1.0.3` : in Betrieb vom 21. bis 27.11.2012
+- :doc:`1.0.4` : war in Betrieb vom 27. bis 30.11.2012
 
-- :doc:`1.0.2` : in Betrieb seit dem 20.11.2012
+- :doc:`1.0.3` : war in Betrieb vom 21. bis 27.11.2012
+
+- :doc:`1.0.2` : war in Betrieb seit dem 20.11.2012
 
 - :doc:`1.0.1` : released am 09.11.2012
 
--- lino_welfare/modlib/pcsw/fixtures/std.py
+++ lino_welfare/modlib/pcsw/fixtures/std.py
@@ -215,13 +215,13 @@ def objects():
       #~ ))
 
     exam_policy = Instantiator('isip.ExamPolicy','every',every_unit=DurationUnits.months).build
-    yield exam_policy(1,calendar=et,start_time="9:00",**babel_values('name',en='every month',de='monatlich',fr="mensuel"))
-    yield exam_policy(2,calendar=et,start_time="9:00",**babel_values('name',en='every 2 months',de='zweimonatlich',fr="bimensuel"))
-    yield exam_policy(3,calendar=et,start_time="9:00",**babel_values('name',en='every 3 months',de='alle 3 Monate',fr="tous les 3 mois"))
+    yield exam_policy(1,calendar=et,start_time="9:00",**babel_values('name',en='every month',de=u'monatlich',fr=u"mensuel"))
+    yield exam_policy(2,calendar=et,start_time="9:00",**babel_values('name',en='every 2 months',de=u'zweimonatlich',fr=u"bimensuel"))
+    yield exam_policy(3,calendar=et,start_time="9:00",**babel_values('name',en='every 3 months',de=u'alle 3 Monate',fr=u"tous les 3 mois"))
     exam_policy = Instantiator('isip.ExamPolicy','every',every_unit=DurationUnits.weeks).build
-    yield exam_policy(2,calendar=et,start_time="9:00",**babel_values('name',en='every 2 weeks',de='zweiwöchentlich',fr="hebdomadaire"))
+    yield exam_policy(2,calendar=et,start_time="9:00",**babel_values('name',en='every 2 weeks',de=u'zweiwöchentlich',fr=u"hebdomadaire"))
     exam_policy = Instantiator('isip.ExamPolicy').build
-    yield exam_policy(0,**babel_values('name',en='other',de='andere',fr="autre"))
+    yield exam_policy(0,**babel_values('name',en='other',de=u'andere',fr=u"autre"))
         
     #~ def create_dsbe_aidtype(id,name,name_fr):
         #~ return AidType(id=id,name=name,name_fr=name_fr)        
