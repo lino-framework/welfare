447ab061a050deb269a5965a16fc8634e42d6d38
Luc Saffre <luc.saffre@gmx.net>
2013-04-12 10:54:46 +0300

    http://code.google.com/p/lino/source/browse/docs/blog/2013/0412.rst

--- fabfile.py
+++ fabfile.py
@@ -5,12 +5,4 @@ setup_from_project('lino_welfare')
 #~ env.django_databases.append('userdocs')
 #~ env.tolerate_sphinx_warnings = True
 
-#~ env.django_doctests.append('tutorials.fixtures1.settings')
-
-#~ env.django_admin_tests.append('tutorials.fixtures1.settings')
-
-# run only these with `fab t4`
-#~ env.docs_doctests.append('tested/misc.rst')
-#~ env.docs_doctests.append('tested/debts.rst')
-
 env.languages = 'fr nl de'.split()
