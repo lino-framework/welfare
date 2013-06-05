91ccae7914550904d057f3afb4176113f12696a1
Luc Saffre <luc.saffre@gmx.net>
2013-06-05 10:22:35 +0300

    http://code.google.com/p/lino/source/browse/docs/blog/2013/0605.rst

--- lino_welfare/__init__.py
+++ lino_welfare/__init__.py
@@ -18,3 +18,4 @@ execfile(os.path.join(os.path.dirname(__file__),'setup_info.py'))
 #~ execfile(os.path.join(os.path.dirname(__file__),'..','setup_info.py'))
 __version__ = SETUP_INFO['version']
 
+intersphinx_url = "http://welfare.lino-framework.org"
