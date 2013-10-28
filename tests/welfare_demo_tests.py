fdb82002a51a0471d62237fab8ff5f31b683f906
Luc Saffre <luc.saffre@gmx.net>
2013-10-28 21:17:29 +0200

    http://code.google.com/p/lino/source/browse/docs/blog/2013/1028.rst

--- tests/welfare_demo_tests.py
+++ tests/welfare_demo_tests.py
@@ -1,11 +1,22 @@
+# -*- coding: UTF-8 -*-
+## Copyright 2013 Luc Saffre
+## This file is part of the Lino-Welfare project.
+## Lino-Welfare is free software; you can redistribute it and/or modify 
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## Lino-Welfare is distributed in the hope that it will be useful, 
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+## GNU General Public License for more details.
+## You should have received a copy of the GNU General Public License
+## along with Lino-Welfare; if not, see <http://www.gnu.org/licenses/>.
+
 """
-
 To run only this test::
-
-  $ cd tests
-  $ export DJANGO_SETTINGS_MODULE=lino_welfare.settings.test
-  $ python -m unittest welfare_demo_tests
  
+  $ go welfare
+  $ python setup.py test -s tests.welfare_demo_tests
 
 """
 from lino.utils.test import DemoTestCase
