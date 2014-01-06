a9ff24019b744ef770b02d72e2609ec264ec76c8
Luc Saffre <luc.saffre@gmail.com>
2014-01-06 07:20:08 +0200

    http://docs.lino-framework.org/blog/2014/0106.html

--- lino_welfare/__init__.py
+++ lino_welfare/__init__.py
@@ -18,4 +18,5 @@ execfile(os.path.join(os.path.dirname(__file__), 'project_info.py'))
 __version__ = SETUP_INFO['version']
 
 intersphinx_url = "http://welfare.lino-framework.org"
+intersphinx_url_userdocs = "http://welfare-user.lino-framework.org"
 srcref_url = 'https://github.com/lsaffre/lino-welfare/blob/master/%s'
