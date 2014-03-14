8c37998c6bc86cb81806cd479445e1eddb0b64d1
Luc Saffre <luc.saffre@gmail.com>
2014-03-14 06:42:23 +0100

    http://docs.lino-framework.org/blog/2014/0314.html

--- lino_welfare/settings/fr/__init__.py
+++ lino_welfare/settings/fr/__init__.py
@@ -28,19 +28,19 @@ class Site(Site):
         from lino import dd
         from django.utils.translation import ugettext_lazy as _
         dd.UserProfiles.reset(
-            '* office coaching integ cbss newcomers reception')
+            '* office coaching integ courses cbss newcomers reception')
         add = dd.UserProfiles.add_item
-        add('000', _("Anonymous"),                   '_ _ _ _ _ _ _',
+        add('000', _("Anonymous"),                   '_ _ _ _ _ _ _ _',
             name='anonymous',
             readonly=True,
             authenticated=False)
-        add('100', _("Integration Agent"),           'U U U U U _ _')
-        add('110', _("Integration Agent (Manager)"), 'U M M M U _ _')
-        add('200', _("Newcomers consultant"),        'U U U _ U U _')
-        add('210', _("Reception clerk"),             'U _ _ _ _ _ U')
-        add('400', _("Social agent"),                'U U U _ U _ _')
-        add('410', _("Social agent (Manager)"),      'U M M _ U _ _')
-        add('900', _("Administrator"),               'A A A A A A A',
+        add('100', _("Integration Agent"),           'U U U U U U _ _')
+        add('110', _("Integration Agent (Manager)"), 'U M M M M U _ _')
+        add('200', _("Newcomers consultant"),        'U U U _ M U U _')
+        add('210', _("Reception clerk"),             'U _ _ _ _ _ _ U')
+        add('400', _("Social agent"),                'U U U _ U U _ _')
+        add('410', _("Social agent (Manager)"),      'U M M _ M U _ _')
+        add('900', _("Administrator"),               'A A A A A A A A',
             name='admin')
 
 
