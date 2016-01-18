ab88cfd7e67e22d2bbc85e4e6e4dce868f4d6d72
Luc Saffre <luc.saffre@gmail.com>
2016-01-18 20:15:51 +0200

    http://luc.lino-framework.org/blog/2016/0118.html

--- lino_welfare/modlib/fse/models.py
+++ lino_welfare/modlib/fse/models.py
@@ -23,11 +23,14 @@ from __future__ import unicode_literals
 from __future__ import print_function
 
 from django.db import models
+from django.db.utils import OperationalError
+from django.conf import settings
 
-from lino.api import dd, _
+from lino.api import rt, dd, _
 from lino import mixins
 
-from .choicelists import ParticipationCertificates
+from lino_welfare.modlib.integ.roles import IntegrationAgent
+from .choicelists import ParticipationCertificates, DossierColumns
 
 
 class Dossier(mixins.DatePeriod):
@@ -37,7 +40,7 @@ class Dossier(mixins.DatePeriod):
         verbose_name_plural = _("FSE Dossiers")
 
     client = dd.ForeignKey('pcsw.Client')
-    academic_level = dd.ForeignKey('cv.EducationLevel')
+    education_level = dd.ForeignKey('cv.EducationLevel')
     children_at_charge = models.BooleanField(
         _("Children at charge"), default=False)
     certified_handicap = models.BooleanField(
@@ -52,6 +55,15 @@ class Dossier(mixins.DatePeriod):
 
 class Dossiers(dd.Table):
     model = 'fse.Dossier'
+    detail_layout = """
+    client children_at_charge certified_handicap other_difficulty id
+    education_level result remark
+    HoursByDossier
+    """
+    insert_layout = """
+    education_level result
+    remark
+    """
 
 
 class AllDossiers(Dossiers):
@@ -59,7 +71,75 @@ class AllDossiers(Dossiers):
 
 
 class DossiersByClient(Dossiers):
+    """Lists the FSE dossiers for a given client."""
     master_key = 'client'
     auto_fit_column_widths = True
+    column_names = "start_date end_date remark *"
+
+
+class HoursByDossier(dd.VirtualTable):
+    required_roles = dd.required(IntegrationAgent)
+    label = _("Hours by dossier")
+    master = 'fse.Dossier'
+
+    slave_grid_format = 'html'
+
+    @classmethod
+    def get_data_rows(self, ar):
+        if ar is None or ar.master_instance is None or ar.param_values is None:
+            return
+        pv = ar.param_values
+        qs = rt.modules.cal.Guest.objects(
+            partner=ar.master_instance,
+            event__date__gte=pv.start_date,
+            event__date__lte=pv.end_date)
+        qs = qs.order_by('event__date')
+        for obj in qs:
+            hours = 1
+            dc = obj.event_type.dossier_column
+            obj._sums.collect(dc, hours)
+            yield obj
+
+    @dd.virtualfield('pcsw.Coaching.user')
+    def user(self, obj, ar):
+        return obj
+
+    @dd.requestfield(_("Total"))
+    def row_total(self, obj, ar):
+        pv = ar.param_values
+        return rt.modules.cal.Guests.request(
+            partner=ar.master_instance,
+            start_date=pv.start_date, end_date=pv.end_date)
+        return obj.my_persons
+
+
+@dd.receiver(dd.post_analyze)
+def on_database_ready(sender, **kw):
+    """
+    Add columns to HoursByDossier from DossierColumns
+
+    This must also be called before each test case.
+    """
+    self = HoursByDossier
+    self.column_names = ''
+    for dc in DossierColumns.objects():
+        def w(dc):
+            def func(self, obj, ar):
+                if ar is None:
+                    return None
+                pv = ar.param_values
+                return rt.modules.cal.Guests.request(
+                    param_values=dict(
+                        partner=ar.master_instance,
+                        dossier_column=dc,
+                        start_date=pv.start_date, end_date=pv.end_date))
+            return func
+        vf = dd.RequestField(w(dc), verbose_name=dc.text)
+        self.add_virtual_field('FSE' + dc.value, vf)
+        self.column_names += ' ' + vf.name
+
+    self.column_names += ' row_total'
+    self.clear_handle()  # avoid side effects when running multiple test cases
+    settings.SITE.resolve_virtual_fields()
 
 
