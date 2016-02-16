6e7c75bf1d02242b1862f9cf61b3e203a9babed9
Luc Saffre <luc.saffre@gmail.com>
2016-02-16 09:16:19 +0200

    http://luc.lino-framework.org/blog/2016/0216.html

--- lino_welfare/projects/chatelet/modlib/courses/models.py
+++ lino_welfare/projects/chatelet/modlib/courses/models.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2014-2015 Luc Saffre
+# Copyright 2014-2016 Luc Saffre
 # This file is part of Lino Welfare.
 #
 # Lino Welfare is free software: you can redistribute it and/or modify
@@ -89,7 +89,7 @@ class Enrolment(Enrolment):
 Enrolments.detail_layout = """
 request_date user
 course pupil
-remark amount workflow_buttons printed
+remark workflow_buttons printed
 motivation problems
 """
 
