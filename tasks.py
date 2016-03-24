9ae71b4222f571e10bc64c3009e5b2f110a367ff
Luc Saffre <luc.saffre@gmail.com>
2016-03-24 16:18:19 +0200

    http://luc.lino-framework.org/blog/2016/0324.html

--- tasks.py
+++ tasks.py
@@ -1,15 +1,7 @@
-from atelier.tasks import *
-env.setup_from_tasks(globals(), "lino_welfare")
+# from atelier.invlib import add_demo_project
+from atelier.tasks import ns, setup_from_tasks
 
-env.add_demo_project('lino_welfare.projects.std.settings.demo')
-env.add_demo_project('lino_welfare.projects.eupen.settings.demo')
-env.add_demo_project('lino_welfare.projects.chatelet.settings.demo')
+setup_from_tasks(globals(), "lino_welfare")
 
-env.locale_dir = 'lino_welfare/modlib/welfare/locale'
-env.languages = ['en', 'de', 'fr']
-env.revision_control_system = 'git'
-env.apidoc_exclude_pathnames = ['lino_welfare/projects']
-env.tolerate_sphinx_warnings = False
-
-env.cleanable_files = ['docs/api/lino_welfare.*']
+ns.configure(dict(languages=['en', 'de', 'fr']))
 
