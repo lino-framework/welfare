f2f8ae4dcd51b181eae2dc61ebe1273590b9d4ef
Luc Saffre <luc.saffre@gmail.com>
2016-03-25 20:18:10 +0200

    http://luc.lino-framework.org/blog/2016/0325.html

--- tasks.py
+++ tasks.py
@@ -1,7 +1,4 @@
 # from atelier.invlib import add_demo_project
-from atelier.tasks import ns, setup_from_tasks
-
-setup_from_tasks(globals(), "lino_welfare")
-
+from atelier.tasks import ns
+ns.setup_from_tasks(globals(), "lino_welfare")
 ns.configure(dict(languages=['en', 'de', 'fr']))
-
