6ae18b84afae2406bf3f7bbdfd43b89964f453e7
Luc Saffre <luc.saffre@gmail.com>
2016-09-02 09:41:21 +0300

    http://luc.lino-framework.org/blog/2016/0902.html

--- lino_welfare/projects/chatelet/settings/demo.py
+++ lino_welfare/projects/chatelet/settings/demo.py
@@ -4,7 +4,7 @@ from lino_welfare.projects.chatelet.settings import *
 
 class Site(Site):
     # title = "Welfare Chatelet Demo"
-    the_demo_date = datetime.date(2014, 05, 22)
+    the_demo_date = datetime.date(2014, 5, 22)
     # ignore_dates_after = datetime.date(2019, 05, 22)
     use_java = False
 
