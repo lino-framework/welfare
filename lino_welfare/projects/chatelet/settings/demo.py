74869476bb7ed57c71be691a680d8286afd94266
Luc Saffre <luc.saffre@gmail.com>
2017-01-04 08:11:59 +0200

    tried new option use_silk_icons

--- lino_welfare/projects/chatelet/settings/demo.py
+++ lino_welfare/projects/chatelet/settings/demo.py
@@ -7,6 +7,7 @@ class Site(Site):
     the_demo_date = datetime.date(2014, 5, 22)
     # ignore_dates_after = datetime.date(2019, 05, 22)
     use_java = False
+    # use_silk_icons = True  # temporarily
 
 
 SITE = Site(globals())
