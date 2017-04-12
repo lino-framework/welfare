8e2e54eb0979223c56ce16251761c4b03f3df262
Luc Saffre <luc.saffre@gmail.com>
2017-04-12 04:48:35 +0300

    http://luc.lino-framework.org/blog/2017/0412.html

--- lino_welfare/modlib/pcsw/actions.py
+++ lino_welfare/modlib/pcsw/actions.py
@@ -80,13 +80,9 @@ class RefuseClient(ChangeStateAction):
             client=obj, user=ar.get_user(), state=self.target_state)
         body = str(ar.action_param_values.reason)
         if ar.action_param_values.remark:
-            body += '\n' + ar.action_param_values.remark
-        kw = dict()
-        kw.update(message=subject)
-        kw.update(alert=_("Success"))
-        dd.logger.info("20170411 %r %r", subject, body)
-        obj.emit_system_note(
-            ar, subject=subject, body=body)
+            body += '\n' + str(ar.action_param_values.remark)
+        dd.logger.info("20170412 %r", ar.action_param_values)
+        obj.emit_system_note(ar, subject=subject, body=body)
         mt = rt.models.notify.MessageTypes.action
         
         if subject:
@@ -95,6 +91,9 @@ class RefuseClient(ChangeStateAction):
             rt.models.notify.Message.emit_message(
                 ar, obj, mt, msg, recipients)
             
+        kw = dict()
+        kw.update(message=subject)
+        kw.update(alert=_("Success"))
         ar.success(**kw)
 
 
