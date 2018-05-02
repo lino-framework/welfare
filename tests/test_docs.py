d85e2cc52025b1b0092e69b7872e8c09a2e06a60
Luc Saffre <luc.saffre@gmail.com>
2018-05-02 07:19:09 +0300

    http://luc.lino-framework.org/blog/2018/0502.html

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,6 +1,6 @@
-from atelier.test import make_docs_suite
-def load_tests(loader, standard_tests, pattern):
-    return make_docs_suite(
-        "docs", addenv=dict(LINO_LOGLEVEL="INFO"))
+# from atelier.test import make_docs_suite
+# def load_tests(loader, standard_tests, pattern):
+#     return make_docs_suite(
+#         "docs", addenv=dict(LINO_LOGLEVEL="INFO"))
 
 
