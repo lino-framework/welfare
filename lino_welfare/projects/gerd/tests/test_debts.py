952a02843d5190c8c3a4cc62d546e498634ba17f
Luc Saffre <luc.saffre@gmail.com>
2019-05-24 08:17:35 +0300

    http://luc.lino-framework.org/blog/2019/0524.html

--- lino_welfare/projects/gerd/tests/test_debts.py
+++ lino_welfare/projects/gerd/tests/test_debts.py
@@ -1,20 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2015-2017 Rumma & Ko Ltd
-# This file is part of Lino Welfare.
-#
-# Lino Welfare is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# Lino Welfare is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public
-# License along with Lino Welfare.  If not, see
-# <http://www.gnu.org/licenses/>.
+# Copyright 2015-2019 Rumma & Ko Ltd
 
 """Some test cases for :mod:`lino_welfare.modlib.debts`.
 
@@ -145,7 +130,7 @@ class DebtsTests(RemoteAuthTestCase):
         url = "/api/debts/Budgets/1?&an=duplicate&sr=1"
         dlg = []
         dlg.append((
-            "This will create a copy of Budget 1 for A & B A-B Are you sure?",
+            "This will create a copy of Budget 1 for A & B A-B. Are you sure?",
             'yes'))
         dlg.append((
             'Duplicated Budget 1 for A & B A-B to Budget 3 for A & B A-B.',
