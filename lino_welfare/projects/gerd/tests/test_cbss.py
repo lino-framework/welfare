51353c2c3eab9dd6f2c9b5b4289f73e2d8bd2109
Luc Saffre <luc.saffre@gmail.com>
2019-06-12 17:16:55 +0300

    http://luc.lino-framework.org/blog/2019/0612.html

--- lino_welfare/projects/gerd/tests/test_cbss.py
+++ lino_welfare/projects/gerd/tests/test_cbss.py
@@ -1,20 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2012-2013 Rumma & Ko Ltd
-# This file is part of Lino Welfare.
-#
-# Lino Welfare is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# Lino Welfare is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public
-# License along with Lino Welfare.  If not, see
-# <http://www.gnu.org/licenses/>.
+# License: BSD (see file COPYING for details)
 
 """
 This module contains "quick" tests that are run on a demo database
