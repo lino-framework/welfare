a3c6ded7e46f8d77b226246bd2169ec2d997a3f6
Tonis Piip <Tonis@eggo.org>
2019-08-05 09:46:54 +0300

    #2948 make migrations before prep test

--- lino_welfare/projects/gerd/settings/demo.py
+++ lino_welfare/projects/gerd/settings/demo.py
@@ -3,6 +3,7 @@ import datetime
 from lino_welfare.projects.gerd.settings import *
 
 class Site(Site):
+    migration_module = "lino_welfare.projects.gerd.migrations"
     the_demo_date = datetime.date(2014, 5, 22)
     # ignore_dates_after = datetime.date(2019, 05, 22)
     use_java = False
