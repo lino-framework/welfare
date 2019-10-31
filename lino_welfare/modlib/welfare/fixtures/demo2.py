22eff92ec6c643ad71555d39f90d4ea933f407e8
Luc Saffre <luc.saffre@gmail.com>
2019-11-01 01:55:53 +0200

    http://luc.lino-framework.org/blog/2019/1101.html

--- lino_welfare/modlib/welfare/fixtures/demo2.py
+++ lino_welfare/modlib/welfare/fixtures/demo2.py
@@ -1,10 +1,10 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2014-2015 Rumma & Ko Ltd
+# Copyright 2014-2019 Rumma & Ko Ltd
 # License: BSD (see file COPYING for details)
 """
 For each client who has a non-empty `card_number` we create a
 corresponding image file in the local media directory in order to
-avoid runtime error when printing documents who include this picture
+avoid runtime error when printing documents thaqt include this picture
 (`eid_content.odt` and `file_sheet.odt`).
 
 """
@@ -14,8 +14,6 @@ import shutil
 
 from lino.api import rt, dd
 
-from lino_xl.lib.beid.mixins import get_image_path
-
 
 def objects():
 
@@ -24,7 +22,7 @@ def objects():
     for obj in Client.objects.exclude(card_number=''):
         fn = obj.get_image_path()
         if not os.path.exists(fn):
-            src = get_image_path(None)
+            src = Client.card_number_to_image_path(None)
             #dd.logger.info("20150531 copy %s to %s...", src, fn)
             rt.makedirs_if_missing(os.path.dirname(fn))
             shutil.copyfile(src, fn)
@@ -33,4 +31,3 @@ def objects():
 
         # else:
            # dd.logger.info("20150531 %s exists", fn)
-    
