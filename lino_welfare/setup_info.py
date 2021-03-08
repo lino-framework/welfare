13b2c15c78e4c535137ae0de3cae57a89fad3181
Luc Saffre <luc.saffre@gmail.com>
2021-03-08 19:09:41 +0200

    release to pypi

--- lino_welfare/setup_info.py
+++ lino_welfare/setup_info.py
@@ -15,7 +15,7 @@ requires.append('suds-py3')
 
 SETUP_INFO = dict(
     name='lino-welfare',
-    version='21.3.0',
+    version='21.3.1',
     install_requires=requires,
     test_suite='tests',
     tests_require=['pytest'],
